1ffecc03cc4da6d21ece687cfe4d84f124051c50
Raja Arslan <rakhan@lmkt.com>
2024-09-24 15:18:37 +0500

    Fixes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prmsc-hr'`, () => {
+  it(`should have as title 'todo-hr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prmsc-hr');
+    expect(app.title).toEqual('todo-hr');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prmsc-hr app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todo-hr app is running!');
   });
 });
